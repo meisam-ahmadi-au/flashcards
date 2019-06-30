f6d0bf155fb8081a215fd6956fd60a3f15cbbb31
DESKTOP-PULU5LM\Meisam <meisam.ahmadi.au@gmail.com>
2019-06-30 18:56:13 +1000

    fix travis-ci bug

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import AddCard from './components/AddCard/AddCard';
-import Authenticate from './components/Authenticate/Authenticate';
 import CategoryContainer from './components/Decks/DeckContainer';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
@@ -12,6 +11,7 @@ const App: React.FC = () => {
   return (
     <BrowserRouter>
       <div className="App">
+        {isAuthenticated ? null : null}
         <Navbar setAuthenticate={setAuthenticate} />
         <Switch>
           <Route path="/addcard" component={AddCard} />
