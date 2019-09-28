bac9adf12eeca981af7f5a6f91265db0af77bbf5
DESKTOP-PULU5LM\Meisam <meisam.ahmadi.au@gmail.com>
2019-09-28 19:54:44 +1000

    Fix travis-ci error

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import './App.css';
 import AddCard from './components/AddCard/AddCard';
 import Cards from './components/Cards/Cards';
